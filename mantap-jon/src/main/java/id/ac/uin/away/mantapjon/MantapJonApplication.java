b22ae3ae44be9ab831c2f1f9a840e66370b42adb
Zahidy <83575905+Zahidy@users.noreply.github.com>
2022-03-10 11:57:30 +0700

    Latihan Spring Boot 1
    gass

---
+++ mantap-jon/src/main/java/id/ac/uin/away/mantapjon/MantapJonApplication.java
@@ -0,0 +1,13 @@
+package id.ac.uin.away.mantapjon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantapJonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantapJonApplication.class, args);
+	}
+
+}
---
+++ mantap-jon/src/test/java/id/ac/uin/away/mantapjon/MantapJonApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.uin.away.mantapjon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantapJonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
